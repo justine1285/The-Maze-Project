a66947228a5a67bc1550dd93c50196a1aac73ed0
Justine1285 <justineadugbo@gmail.com>
2024-05-31 02:02:45 -0700

    walls

--- src/walls.c
+++ src/walls.c
@@ -39,12 +39,12 @@ void renderFloor(int wallBottomPixel, color_t *texelColor, int x)
 		distance = (ratio * PROJ_PLANE)
 					/ cos(rays[x].rayAngle - player.rotationAngle);
 
-		textureOffsetY = (int)abs((distance * sin(rays[x].rayAngle)) + player.y);
-		textureOffsetX = (int)abs((distance * cos(rays[x].rayAngle)) + player.x);
+		textureOffsetY = (int)fabs((distance * sin(rays[x].rayAngle)) + player.y);
+		textureOffsetX = (int)fabs((distance * cos(rays[x].rayAngle)) + player.x);
 
-		textureOffsetX = (int)(abs(textureOffsetX * texture_width / 30)
+		textureOffsetX = (int)(fabs(textureOffsetX * texture_width / 30)
 								% texture_width);
-		textureOffsetY = (int)(abs(textureOffsetY * texture_height / 30)
+		textureOffsetY = (int)(fabs(textureOffsetY * texture_height / 30)
 								% texture_height);
 
 		*texelColor = wallTextures[4].
@@ -75,12 +75,12 @@ void renderCeil(int wallTopPixel, color_t *texelColor, int x)
 		distance = (ratio * PROJ_PLANE)
 					/ cos(rays[x].rayAngle - player.rotationAngle);
 
-		textureOffsetY = (int)abs((-distance * sin(rays[x].rayAngle)) + player.y);
-		textureOffsetX = (int)abs((-distance * cos(rays[x].rayAngle)) + player.x);
+		textureOffsetY = (int)fabs((-distance * sin(rays[x].rayAngle)) + player.y);
+		textureOffsetX = (int)fabs((-distance * cos(rays[x].rayAngle)) + player.x);
 
-		textureOffsetX = (int)(abs(textureOffsetX * texture_width / 40)
+		textureOffsetX = (int)(fabs(textureOffsetX * texture_width / 40)
 								% texture_width);
-		textureOffsetY = (int)(abs(textureOffsetY * texture_height / 40)
+		textureOffsetY = (int)(fabs(textureOffsetY * texture_height / 40)
 								% texture_height);
 
 		*texelColor = wallTextures[6].
